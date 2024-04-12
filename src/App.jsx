c4f013a84022d59842d289dce7bde6b3ce6fc25b
denystulupov <tulupovdenys@gmail.com>
2024-04-12 12:51:09 +0300

    File structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,43 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+import Home from "./ui/Home";
+import Error from "./ui/Error";
+import Menu, { loader as menuLoader } from "./features/menu/Menu";
+import Cart from "./features/cart/Cart";
+import CreateOrder, { action as createOrderAction } from "./features/order/CreateOrder";
+import Order, { loader as orderLoader } from "./features/order/Order";
+import AppLayuot from "./ui/AppLayuot";
+
+const router = createBrowserRouter([
+  {
+    element: <AppLayuot />,
+    errorElement: <Error />,
+    children: [
+      { path: "/", element: <Home /> },
+      {
+        path: "/menu",
+        element: <Menu />,
+        loader: menuLoader,
+        errorElement: <Error />,
+      },
+      { path: "/cart", element: <Cart /> },
+      {
+        path: "/order/new",
+        element: <CreateOrder />,
+        action: createOrderAction,
+      },
+      {
+        path: "/order/:orderId",
+        element: <Order />,
+        loader: orderLoader,
+        errorElement: <Error />,
+      },
+    ],
+  },
+]);
+
 function App() {
-  return <div>Hello Vite!!!</div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
